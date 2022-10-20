9488e4f3c02f1aba159a51b62196cfbb43e5db36
Jose Carlos Sáez Fernández <jcsaezfernandez@gmail.com>
2022-10-20 20:39:49 +0200

    initial repositorio

---
+++ routes/index.routes.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const router = express.Router();
+
+/* GET home page */
+router.get("/", (req, res, next) => {
+  res.render("index");
+});
+// RUTAS DE AUTENTICACIÓN AUTH
+
+const authRoutes = require("./auth.routes.js")
+router.use("/auth", authRoutes)
+
+
+
+// RUTAS DE BLOG
+
+const blogRoutes = require("./blog.routes.js")
+router.use("/blog", blogRoutes)
+
+// RUTAS DE COLECTION
+
+const colectionRoutes = require("./colection.routes.js")
+router.use("/colection", colectionRoutes)
+
+// RUTAS DE PRODUCT
+
+const productRoutes = require("./prouduct.routes.js")
+router.use("/product", productRoutes)
+
+// RUTAS DE PROFILE
+
+const profileRoutes = require("./profile.routes")
+router.use("/profile", productRoutes)
+
+
+
+module.exports = router;
