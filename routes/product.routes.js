b78b9771ee3a912a32fde7c2a739490d9a3239bd
Curro Garcia <curro-91@hotmail.com>
2022-10-21 18:13:51 +0200

    ruta de producto, aun sin acabar

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -1,4 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const Product = require("../models/Product.model.js")
+
+//GET ruta para crear un producto
+router.get("/create", (req, res, next) => {
+  res.render("product/create.hbs")
+})
+
+//GET ruta donde el admin puede ver todos los productos
+router.get("/", (req, res, next) => {
+
+  Product.find()
+  .then((listProduct) => {
+    res.render("product/list.hbs", {
+      listProduct
+    })
+  })
+  .catch((err) => {
+    next(err)
+  })
+
+})
 
 module.exports = router;
