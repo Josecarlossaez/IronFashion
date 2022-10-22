d145ccfc54736f3ea0664f3a28d4b36993368b16
Jose Carlos Sáez Fernández <jcsaezfernandez@gmail.com>
2022-10-22 14:28:27 +0200

    rutas create y list product y colection

--- routes/colection.routes.js
+++ routes/colection.routes.js
@@ -1,4 +1,46 @@
 const express = require('express');
 const router = express.Router();
+const Collection = require("../models/Collection.model.js")
+const Product = require("../models/Product.model.js")
+// GET ("/colection/create")
+router.get("/create", (req, res, next) => {
+    Product.find()
+    .then((productos) => {
+        res.render("colection/create.hbs",{
+            productList:productos
+        })
+    }).catch((err) =>{
+        next(err)
+    })
+})
+
+//POST ("/colection/create")
+router.post("/create", (req,res,next) =>{
+    const{title, description, productos} = req.body
+    const colectiontoAdd ={title,description,productos}
+    Collection.create(colectiontoAdd)
+    .then((colection) => {
+        res.redirect("/colection/list")
+    }).catch((err) => {
+        next(err)
+    })
+
+})
+
+// VISUALIZAR LISTA DE COLECCIONES
+// GET ("/colection/list")
+router.get("/list", (req, res, next) => {
+   Collection.find()
+   .populate("productos")
+   .then((colection) => {
+    res.render("colection/list.hbs",{
+        colectionList : colection
+    })
+
+   }).catch((err) => {
+    next(err)
+   })
+})
+
 
 module.exports = router;
--- routes/product.routes.js
+++ routes/product.routes.js
@@ -2,13 +2,27 @@ const express = require('express');
 const router = express.Router();
 const Product = require("../models/Product.model.js")
 
-//GET ruta para crear un producto
+//GET  ("/product/create") ruta para visualizar la página para crear un producto
 router.get("/create", (req, res, next) => {
   res.render("product/create.hbs")
+});
+
+// POST ("/product/create") ruta para crear un producto
+router.post("/create", async(req, res, next) => {
+   const{name, cost, description, type,img} = req.body
+  try{
+   
+    const productToAdd ={name,cost,description,type,img}
+    await Product.create(productToAdd);
+    res.redirect("/list")
+
+  }catch (err){
+    next(err)
+  }
 })
 
-//GET ruta donde el admin puede ver todos los productos
-router.get("/", (req, res, next) => {
+//GET ("/produtct/list")ruta donde el admin puede ver todos los productos
+router.get("/list", (req, res, next) => {
 
   Product.find()
   .then((listProduct) => {
@@ -19,6 +33,23 @@ router.get("/", (req, res, next) => {
   .catch((err) => {
     next(err)
   })
+
+})
+
+// GET ("/product/:productId/details")
+router.get("/:productId/details",  (req, res, next) => {
+  const {productId} = req.params;
+
+   Product.findById(productId)
+   .then((response) => {
+     res.render("product/details.hbs", {
+      productDetails:response
+    })
+
+   }).catch((err)=>{
+    next(err)
+   })
+   
 
 })
 
