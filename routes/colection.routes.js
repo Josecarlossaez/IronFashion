3e27270b5620adba75e040f26b0cb6bed4c3c532
Curro Garcia <curro-91@hotmail.com>
2022-10-28 11:06:46 +0200

    css de busqueda y retocando el css de favorito

--- routes/colection.routes.js
+++ routes/colection.routes.js
@@ -101,11 +101,8 @@ router.get("/:colectionId/details",isLoggedIn, async (req, res, next) => {
 
     try {
         const colectionDetails = await Collection.findById(colectionId).populate("productos")
-        // const allProducts = await Product.find()
-        //console.log(allProducts)
         res.render("colection/details.hbs", {
-        colectionDetails
-    //    allProducts 
+        colectionDetails 
     })
         
     } catch (error) {
